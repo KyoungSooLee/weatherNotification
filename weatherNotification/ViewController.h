f7017f357ea0a59e31e384794cd11b96feb2b06d
이경수 <bingo@igyeongsuui-imac.local>
2016-10-27 11:13:30 +0900

    Initial Commit

---
+++ weatherNotification/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  weatherNotification
+//
+//  Created by bingo on 2016. 10. 27..
+//  Copyright © 2016년 bingo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ weatherNotification/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  weatherNotification
+//
+//  Created by bingo on 2016. 10. 27..
+//  Copyright © 2016년 bingo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
